3c4b55d5ba61570e4832c61686d0138d436c54f2
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-01-27 11:20:41 +0100

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+    return (
+      <div>
+        <header className="header">
+          <h1 className="title">AdaTasks</h1>
+      </header>
+      </div>
+    )
+  }
+  
+  export default App
+  
